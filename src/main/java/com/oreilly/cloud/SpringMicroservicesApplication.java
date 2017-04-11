5b12fbf69e7966ac5dc7908eb6f48ec64e35f4d9
Jose Fernando Falcao <jffalcao@zeus.jfrf-labs.local>
2017-04-10 21:56:43 -0400

    Autoconfiguration

--- src/main/java/com/oreilly/cloud/SpringMicroservicesApplication.java
+++ src/main/java/com/oreilly/cloud/SpringMicroservicesApplication.java
@@ -4,21 +4,37 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 @RestController
 @Import(DemoCofiguration.class)
 @ImportResource("simple-context.xml")
 public class SpringMicroservicesApplication {
+	
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+	
+	@Bean
+	public ServletRegistrationBean registration(){
+		ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet(), "/servlet/*");
+		registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME);
+		return registration;
+	}
 	
 	@Autowired
 	@Resource(name="circus")
